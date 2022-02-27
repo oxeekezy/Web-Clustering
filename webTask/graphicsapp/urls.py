6db30fc395a6a76af243a7ad80fb9990a58d213d
oxeekezy <62114147+oxeekezy@users.noreply.github.com>
2022-02-27 16:10:06 +0300

    new path's

--- webTask/graphicsapp/urls.py
+++ webTask/graphicsapp/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('bokeh', views.bokeh),
+    path('means-dwt-year', views.dwt_year),
+    path('means-dwt-size', views.dwt_size),
+    path('means-size-year', views.size_year),
+
+    path('dwt-year', views.dwt_year),
+    path('dwt-year', views.dwt_year),
+    path('dwt-year', views.dwt_year),
+
 ]
--- webTask/graphicsapp/views.py
+++ webTask/graphicsapp/views.py
@@ -10,11 +10,18 @@ def index(request):
     return render(request, 'graphicsapp/index.html')
 
 
-def bokeh(request):
+def dwt_year(request):
     plot = kmeans.Clustering(4, 6, 7, 'DWT', 'YEAR')
-
     script, div = components(plot)
-
     return render(request, 'graphicsapp/bokeh.html', {'script': script, 'div': div})
 
 
+def dwt_size(request):
+    plot = kmeans.Clustering(4, 6, 5, 'DWT', 'SIZE')
+    script, div = components(plot)
+    return render(request, 'graphicsapp/bokeh.html', {'script': script, 'div': div})
+
+def size_year(request):
+    plot = kmeans.Clustering(5, 5, 7, 'SIZE', 'YEAR')
+    script, div = components(plot)
+    return render(request, 'graphicsapp/bokeh.html', {'script': script, 'div': div})
